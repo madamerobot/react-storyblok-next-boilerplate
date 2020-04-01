fc941a595e4248f9494812ca1eff3c22c0081bb3
Valerie Fuchs <valerie@yourmajesty.co>
2020-04-01 11:31:39 +0200

    Simplifying markdown transformation

--- components/modules/ParagraphModule/index.jsx
+++ components/modules/ParagraphModule/index.jsx
@@ -2,9 +2,12 @@
 import style from "./index.scss";
 
 const ParagraphModule = ({ content }) => {
+  /* This is Reacts recommended way of injecting pure HTML into your application */
   function createMarkup() {
     return { __html: content };
   }
+
+  /* All we do here is to add Markup (HTML) into our app */
   return (
     <div className={style.root}>
       <div dangerouslySetInnerHTML={createMarkup()} />
--- pages/[page].js
+++ pages/[page].js
@@ -20,25 +20,28 @@ function fetchUrl(url) {
 the url and therefore the page request, e.g. your-site.com/about */
 const SlugPage = ({ data }) => {
   const {
-    content: { body }
+    content: { components }
   } = data;
-  const headlineModuleData = body.find(
+  console.log(data);
+  /* Filtering only data for the Headline module */
+  const headlineModuleData = components.find(
     item => item.component === "Headline Module"
   );
-  const paragraphModuleData = body.find(
+  /* Filtering only data for the Paragraph module */
+  const paragraphModuleData = components.find(
     item => item.component === "Paragraph Module"
   );
-  const paragraphContent = paragraphModuleData.copy.content.map(contentItem =>
-    marked(contentItem.content[0].text)
-  );
-  /* Now you need to map your own components, I just left the headline module as reference */
+  /* With our dependency 'marked' we are turning Contentfuls
+  Markdown into HTML Markup */
+  const paragraphModuleDataMarkUp = marked(paragraphModuleData.copy);
+
   return (
     <DefaultLayout>
       {headlineModuleData ? (
         <HeadlineModule title={headlineModuleData.title} />
       ) : null}
       {paragraphModuleData ? (
-        <ParagraphModule content={paragraphContent} />
+        <ParagraphModule content={paragraphModuleDataMarkUp} />
       ) : null}
     </DefaultLayout>
   );
